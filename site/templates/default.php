4322e1c3adc4b008c45e9ea3b42100f8e69a4663
Brandon Espinoza <brandonespinoza@MacBookAir.attlocal.net>
2025-07-27 00:46:45 -0500

    created new snippets for the header, nav, and footer. Also trying to load an image field into inline html css as a background image

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php snippet('header') ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $page->title() ?></title>
-    <?= css([
-        'assets/css/bootstrap.min.css',
-        'assets/css/main.css'
-    ]) ?>
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="<?= $site->url() ?>"> <?= $site->title() ?> </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <?php foreach ($site->children()->listed() as $item): ?>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><?= $item->title() ?></a>
-                    </li>
-                    <?php endforeach ?>
-                    
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container">
+    <div class="container" style="background-image: url('<?= $page->Herobgimg()->url() ?>'); background-size: cover; background-position: center;">
         <h2><?= $page->Herotext() ?></h2>
         <h2><?= $page->Herosubtitle() ?></h2>
     </div>
-</body>
 
-</html>
+<?php snippet('footer') ?>
