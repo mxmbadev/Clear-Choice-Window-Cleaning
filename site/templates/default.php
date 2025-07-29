69d6c08f4db90c47b0a7a53e878d19418d2da51c
Brandon Espinoza <brandonespinoza@MacBookAir.attlocal.net>
2025-07-29 00:45:42 -0500

    styled hero section and added lato web font

--- site/templates/default.php
+++ site/templates/default.php
@@ -3,7 +3,8 @@
 <section id="heroImage" style="background-image: url('<?= $page->Herobgimg()->toFile()->url() ?>');">
     <div class="container">
         <h2 class="h1 text-white"><?= $page->Herotext() ?></h2>
-        <h3 class="text-white"><?= $page->Herosubtitle() ?></h3>
+        <h3 class="h5 text-white pt-1"><?= $page->Herosubtitle() ?></h3>
+        <hr>
     </div>
 </section>
 
